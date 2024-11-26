e21c120e3a46ff0a5bd7e0ed99f6d4140e427499
Derrick James <derrickjames@gmail.com>
2024-11-26 13:26:06 -0500

    Update script.js

--- page/script.js
+++ page/script.js
@@ -6,7 +6,7 @@ document.getElementById("startScan").addEventListener("click", () => {
       console.log(`QR Code detected: ${decodedText}`);
       // Assuming the QR code contains the student's email address
       sendLogData(decodedText, 'checkout');
-      html5QrCode.stop(); // Stop the QR code scanning after successful scan
+      html5QrCode.stop(); // Stop the QR code scanning after a successful scan
     },
     (errorMessage) => {
       console.error(errorMessage);
@@ -23,17 +23,19 @@ document.getElementById("stopScan").addEventListener("click", () => {
 });
 
 function sendLogData(email, action) {
+  // The data object that will be sent to the Google Apps Script
   const data = {
     email: email,
     action: action
   };
   
+  // Replace 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL' with your actual web app URL
   fetch('https://script.google.com/macros/s/AKfycbwJw23qGa91dz6yg5YGWsVsYD6dumMdJnOljI_Pwqb9isjgZ9HIJStdBlxz0s6xjdlm/exec', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(data),  // Send the data as a JSON string
   })
   .then(response => response.text())
   .then(data => {
